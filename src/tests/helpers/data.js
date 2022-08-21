25f07a344cbfd93843cc17a7d4eb12d9e179ad54
Miguel Ângelo <miguelcg958@gmail.com>
2022-08-21 14:38:32 -0300

    inicio parte 2

---
+++ src/tests/helpers/data.js
@@ -0,0 +1,22 @@
+const repositories = [
+  {
+    id: 1,
+    full_name: 'test/repo_1',
+    description: 'Descrição repo 1',
+    html_url: 'repo 1 html url',
+  },
+  {
+    id: 2,
+    full_name: 'test/repo_2',
+    description: 'Descrição repo 2',
+    html_url: 'repo 2 html url',
+  },
+  {
+    id: 3,
+    full_name: 'test/repo_3',
+    description: 'Descrição repo 3',
+    html_url: 'repo 3 html url',
+  },
+];
+
+export default repositories;
