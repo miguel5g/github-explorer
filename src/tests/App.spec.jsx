011abbb94f8ab2dca41d0f0a1f400c443110afce
Miguel Ângelo <miguelcg958@gmail.com>
2022-08-22 14:05:35 -0300

    finalizado parte 2

--- src/tests/App.spec.jsx
+++ src/tests/App.spec.jsx
@@ -1,5 +1,40 @@
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { screen, waitFor } from '@testing-library/react';
+
+import renderWithRouter from './helpers/renderWithRouter';
+import App from '../App';
+
 describe('<App />', () => {
-  it('todo', () => {
-    /** @todo */
+  it('Testa se a página Search é a primeira a ser renderizada', () => {
+    renderWithRouter(<App />);
+
+    const getTitle = screen.getByRole('heading', { level: 1, name: /Github Explorer/i });
+
+    expect(getTitle).toBeInTheDocument();
+    // expect(history.location.pathname).toBe('/')
+  });
+
+  it('Testar se a página FavoriteRepositories é renderizado quando acessado url "/repositories/favorites"', async () => {
+    const { history } = renderWithRouter(<App />);
+
+    const urlFavorites = '/repositories/favorites';
+
+    history.push(urlFavorites);
+
+    await waitFor(() =>
+      expect(
+        screen.queryByRole('heading', { level: 1, name: /Github Explorer/i })
+      ).not.toBeInTheDocument()
+    );
+
+    expect(history.location.pathname).toBe(urlFavorites);
+
+    const getFavoriteTitle = screen.getByRole('heading', {
+      level: 1,
+      name: /favoritos/i,
+    });
+
+    expect(getFavoriteTitle).toBeInTheDocument();
   });
 });
--- src/tests/Repositories.spec.jsx
+++ src/tests/Repositories.spec.jsx
@@ -1,5 +1,47 @@
+import React from 'react';
+import { screen, waitFor } from '@testing-library/react';
+
+import Repositories from '../pages/Repositories';
+import renderWithRouter from './helpers/renderWithRouter';
+import repositories from './helpers/data';
+
 describe('<Repositories />', () => {
-  it('todo', () => {
-    /** @todo */
+  beforeEach(() => {
+    global.fetch = jest.fn().mockResolvedValue({
+      json: jest.fn().mockResolvedValue(repositories),
+    });
+  });
+
+  test('Testando se a pagina é renderizada', async () => {
+    renderWithRouter(<Repositories match={{ params: { login: 'turma_23' } }} />);
+
+    await waitFor(() =>
+      expect(screen.queryByText('Carregando...')).not.toBeInTheDocument()
+    );
+
+    expect(screen.getByRole('heading', { name: /repositórios/i })).toBeInTheDocument();
+  });
+
+  test('Testa se renderiza os dados da api em tela', async () => {
+    renderWithRouter(<Repositories match={{ params: { login: 'turma_23' } }} />);
+
+    await waitFor(() =>
+      expect(screen.queryByText('Carregando...')).not.toBeInTheDocument()
+    );
+
+    repositories.forEach((repo, index) => {
+      const repoTitle = screen.getByRole('heading', {
+        level: 2,
+        name: repositories[index].full_name,
+      });
+      const repoDescription = screen.getByText(repo.description);
+      const repoLink = screen.getAllByRole('link', { name: /ver no github/i });
+
+      expect(repoTitle).toBeInTheDocument();
+      expect(repoDescription).toBeInTheDocument();
+      expect(repoLink[index]).toHaveAttribute('href', repo.html_url);
+    });
+
+    // expect(repoLink).toHaveLength(3)
   });
 });
